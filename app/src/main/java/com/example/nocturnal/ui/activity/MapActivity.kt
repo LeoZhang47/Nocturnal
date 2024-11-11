d2041533e8aea4719bb008369bb223828a6f6f72
LeoZhang47 <zleo200347@gmail.com>
2024-11-11 13:46:19 -0500

    fix location puck and remove video button

--- app/src/main/java/com/example/nocturnal/ui/activity/MapActivity.kt
+++ app/src/main/java/com/example/nocturnal/ui/activity/MapActivity.kt
@@ -4,6 +4,9 @@ import LocationService
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +16,11 @@ import com.example.nocturnal.data.model.distanceTo
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.plugin.locationcomponent.LocationComponentPlugin
+import com.mapbox.maps.plugin.locationcomponent.location
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mapbox.maps.ImageHolder
+import com.mapbox.maps.plugin.LocationPuck2D
 
 class MapActivity : AppCompatActivity() {
 
@@ -47,18 +54,25 @@ class MapActivity : AppCompatActivity() {
             }
         }
 
+        // Initialize the LocationComponent with custom puck
+        initLocationComponent()
+
         // Request location permissions and start location updates
         requestLocationPermissionAndInitLocation()
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-
-        // Set Camera as the selected item when returning to CameraActivity
-        bottomNavigationView.selectedItemId = R.id.navigation_map
+    private fun initLocationComponent() {
+        val locationComponent = mapView.location.apply {
+            enabled = true
+            // Check if LocationPuck2D is available directly
+            locationPuck = LocationPuck2D(
+                bearingImage = ImageHolder.from(R.drawable.ic_bearing_puck),
+                shadowImage = ImageHolder.from(R.drawable.ic_shadow_puck),
+                topImage = ImageHolder.from(R.drawable.ic_location_puck)
+            )
+        }
     }
+
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -95,7 +109,6 @@ class MapActivity : AppCompatActivity() {
 
     private fun zoomToCurrentLocation(point: Point) {
         val currentCameraPosition = mapView.mapboxMap.cameraState.center
-        // Update camera only if the distance exceeds 10 meters
         if (point.distanceTo(currentCameraPosition) > 10.0) {
             mapView.mapboxMap.setCamera(
                 CameraOptions.Builder()
